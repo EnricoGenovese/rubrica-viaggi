4f34623bf8a0f4f77b6e816558e6c987ad1b666e
EnricoGeno <enricogeno@libero.it>
2025-02-03 09:43:50 +0100

    fix client name;

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,10 +7,9 @@ function Header() {
 
             <div className="">
 
-                <NavLink to={`/`} className="d-flex align-items-center gap-3 text-decoration-none">
+                <NavLink to={`/`} className="d-flex align-items-center gap-3 text-decoration-none border-none">
                     <img src="https://img.icons8.com/?size=100&id=Tu5w6McTElFO&format=png&color=000000" alt="logo-viaggi" style={{ width: "50px", height: "50px" }} />
-                    <h2 className="text-white">TravelDreams</h2>
-
+                    <h2 className="text-white text-center w-100">Booroad</h2>
                 </NavLink>
 
             </div>
