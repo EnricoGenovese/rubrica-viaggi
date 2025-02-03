aa45192c857fdd131821caa6d3ca05dd99616b10
EnricoGeno <enricogeno@libero.it>
2025-02-03 15:54:00 +0100

    btn home centered;

--- src/pages/Travels.jsx
+++ src/pages/Travels.jsx
@@ -13,9 +13,11 @@ export default function Travels() {
 
   return (
     <>
-      <Link to={"/alltravelers"} className="btn btn-secondary">
-        Tutti i viaggatori
-      </Link>
+      <div className="d-flex align-items-center justify-content-center my-3">
+        <Link to={"/alltravelers"} className="btn btn-dark">
+          Tutti i viaggatori
+        </Link>
+      </div>
       <div className="container mt-5">
         <div className="bg-light p-2">
           <h2>Viaggi in corso: </h2>
